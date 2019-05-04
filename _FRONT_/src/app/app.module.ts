9da713d217e94357e18298e4d54856d9227b07d8
TheBuyna <thebuyna@gmail.com>
2019-05-04 21:25:58 +0200

    added top-news component: styled and animated

--- _FRONT_/src/app/app.module.ts
+++ _FRONT_/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNewsComponent } from './top-news/top-news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNewsComponent
   ],
   imports: [
     BrowserModule
