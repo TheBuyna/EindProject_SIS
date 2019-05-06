f8649b83283c8f7b20aaafb158ec8a588fa5898f
sakhash <saker.hashem@gmail.com>
2019-05-07 00:27:27 +0200

    navbar update+scroll-up

--- _FRONT_/src/app/app.module.ts
+++ _FRONT_/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AngularFontAwesomeModule} from 'angular-font-awesome';
 import { FooterComponent } from './footer/footer.component';
+import { ScrollTopComponent } from './scroll-top/scroll-top.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FooterComponent } from './footer/footer.component';
     TopNewsComponent,
     RightSidebarComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ScrollTopComponent
   ],
   imports: [
     BrowserModule,
