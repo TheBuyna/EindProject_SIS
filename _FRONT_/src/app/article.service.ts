bc0aec5470fb1b57744342e66b974c05daa21223
TheBuyna <thebuyna@gmail.com>
2019-05-17 15:40:57 +0200

    added news api for basic use

---
+++ _FRONT_/src/app/article.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  constructor(private http:HttpClient) {}
+
+  API_KEY = '8242b00c3eee4bf794d4bf1d138ee112';
+
+   get_article(searchQuery: string) {
+      let replacedQuery = searchQuery.split(' ').join('-')
+      let articles = 'https://newsapi.org/v2/everything?q=' + replacedQuery + '&sortBy=publishedAt&apiKey=' + this.API_KEY;
+      return this.http.get(articles);
+   }
+
+   get_top_headlines(country: string){
+     let headLineArticle = 'https://newsapi.org/v2/top-headlines?country=' + country + '&language=nl&apiKey=' + this.API_KEY;
+     return this.http.get(headLineArticle);
+   }
+
+   get_article_category(category: string, country: string) {
+    let articles = 'https://newsapi.org/v2/top-headlines?country=' + country + '&category=' + category + '&apiKey=' + this.API_KEY;
+    return this.http.get(articles);
+ }
+
+}
