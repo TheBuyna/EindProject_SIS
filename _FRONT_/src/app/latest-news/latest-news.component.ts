3cc60e6778613578bc7cc70fed7dab15467825b6
TheBuyna <thebuyna@gmail.com>
2019-05-19 17:13:38 +0200

    added small fix for articles

--- _FRONT_/src/app/latest-news/latest-news.component.ts
+++ _FRONT_/src/app/latest-news/latest-news.component.ts
@@ -11,13 +11,14 @@ export class LatestNewsComponent implements OnInit {
 
   constructor(private articleService:ArticleService) { }
 
+  allArticles;
   ngOnInit() {
     this.get_articles();
   }
 
   get_articles() {
-    this.articleService.get_top_headlines('nl').subscribe((result) => {
-        this.allArticles = result.articles;
+    this.articleService.get_top_headlines('us', 'entertainment').subscribe((result) => {
+        this.allArticles = result["articles"];
          console.log(result);
     });
   }
