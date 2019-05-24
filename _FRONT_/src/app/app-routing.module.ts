1dff83f849b32f072ff3f6e8ab45a169e0d762ad
TheBuyna <thebuyna@gmail.com>
2019-05-24 11:15:01 +0200

    fixed small issue

--- _FRONT_/src/app/app-routing.module.ts
+++ _FRONT_/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { CategoryComponent } from "./category/category.component";
+import { CategoryComponent } from './category/category.component';
 
 const routes: Routes = [
   { path: 'category', component: CategoryComponent },
