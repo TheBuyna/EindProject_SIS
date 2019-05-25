02b2d8c0b90d657405415683b6678f2bf77498ff
TheBuyna <thebuyna@gmail.com>
2019-05-25 15:38:32 +0200

    added a little routings

--- _FRONT_/src/app/app-routing.module.ts
+++ _FRONT_/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
+import { HomeMainArticlesComponent } from './home-main-articles/home-main-articles.component';
 
 const routes: Routes = [
   { path: 'category', component: CategoryComponent },
+  { path: 'home', component: HomeMainArticlesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
